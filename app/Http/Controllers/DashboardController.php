08e77bcbfe1ebe2121987a409782381eacec291b
dregore <1876211+dregore@users.noreply.github.com>
2026-03-04 17:55:44 +0300

    fix: add users list for admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index()
 {
-    $user = auth()->user();
-    return view('dashboard', compact('user'));
+    $users = collect();
+    if (Auth::user()->isAdmin()) {
+        $users = User::all();
+    }
+
+    //$user = auth()->user();
+    return view('dashboard', compact('users'));
 }
 }
