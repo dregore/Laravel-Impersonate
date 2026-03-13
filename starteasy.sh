08d5b7bfd5b2f36dd0b9feb3e343cded777ca3ec
dregore <1876211+dregore@users.noreply.github.com>
2026-03-13 15:39:18 +0300

    fix: setup script

--- starteasy.sh
+++ starteasy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 is_installed() {
     command -v "$1" >/dev/null 2>&1
 }
@@ -14,16 +12,23 @@ else
     sudo usermod -aG docker $USER
 fi
 
-echo "--- Updating package list and installing system dependencies ---"
-sudo apt update -y
-sudo apt install -y software-properties-common unzip curl git
+#echo "--- Updating package list and installing system dependencies ---"
+#sudo apt update -y
+#sudo apt install -y software-properties-common unzip curl git
+#sleep 5
 
 echo "--- Installing Laravel framework ---"
-git clone https://github.com/dregore/Laravel-Impersonate.git
-cd Laravel-Impersonate
+git clone https://github.com/dregore/Laravel-Impersonate.git && cd Laravel-Impersonate
 cp .env.example .env
+echo "--- DOCKER BUILD ---"
 sudo docker compose up -d --build
+echo "--- Waiting for services to stabilize (15s) ---"
+sleep 15
+
+echo "--- BREEZE ---"
 sudo docker compose exec app php artisan breeze:install blade --no-interaction
+sleep 5
+echo "--- COPY COPY ---"
 cp routes/web.php.original routes/web.php
 cp resources/views/dashboard.blade.php.original resources/views/dashboard.blade.php
 
